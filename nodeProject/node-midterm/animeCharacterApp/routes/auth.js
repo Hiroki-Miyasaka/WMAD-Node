49e9d2bd2eb25eb005baf3c60869f019834f230d
Hiroki <h.m.jojo.8258@gmail.com>
2023-01-25 17:52:28 -0800

    made header

--- nodeProject/node-midterm/animeCharacterApp/routes/auth.js
+++ nodeProject/node-midterm/animeCharacterApp/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import CharacterInfo from '../models/character.model.js';
-import imageUrl from '../cloud/cloudinary';
+import imageUrl from './cloud/cloudinary';
 
 const router = express.Router();
 
